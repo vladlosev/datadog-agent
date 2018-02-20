6c9e8e72d90aa9e64068163491e6568e1d39ffb6
charlyF <charly@datadoghq.com>
2018-02-19 23:44:06 -0500

    updating integration test, adding relnote

--- test/integration/util/kube_apiserver/apiserver_test.go
+++ test/integration/util/kube_apiserver/apiserver_test.go
@@ -9,7 +9,6 @@
 package kubernetes
 
 import (
-	"context"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -22,7 +21,6 @@ import (
 
 	"github.com/DataDog/datadog-agent/pkg/config"
 	"github.com/DataDog/datadog-agent/pkg/util/kubernetes/apiserver"
-	"github.com/ericchiang/k8s"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
@@ -87,12 +85,10 @@ func (suite *testSuite) SetupTest() {
 
 func (suite *testSuite) TestKubeEvents() {
 	// Init own client to write the events
-	var k8sConf *k8s.Config
-	k8sConf, err := apiserver.ParseKubeConfig(suite.kubeConfigPath)
+	config.Datadog.Set("kubernetes_kubeconfig_path", suite.kubeConfigPath)
+	core, err := apiserver.GetCoreV1Client()
 	require.Nil(suite.T(), err)
-	rawClient, err := k8s.NewClient(k8sConf)
-	require.Nil(suite.T(), err)
-	core := rawClient.CoreV1()
+
 	require.NotNil(suite.T(), core)
 
 	// Ignore potential startup events
@@ -101,8 +97,8 @@ func (suite *testSuite) TestKubeEvents() {
 
 	// Create started event
 	testReference := createObjectReference("default", "integration_test", "event_test")
-	startedEvent := createEvent("default", "test_started", "started", testReference)
-	_, err = core.CreateEvent(context.Background(), startedEvent)
+	startedEvent := createEvent("default", "test_started", "started", *testReference)
+	_, err = core.Events("default").Create(startedEvent)
 	require.Nil(suite.T(), err)
 
 	// Test we get the new started event
@@ -110,11 +106,11 @@ func (suite *testSuite) TestKubeEvents() {
 	require.Nil(suite.T(), err)
 	assert.Len(suite.T(), added, 1)
 	assert.Len(suite.T(), modified, 0)
-	assert.Equal(suite.T(), "started", *added[0].Reason)
+	assert.Equal(suite.T(), "started", added[0].Reason)
 
 	// Create tick event
-	tickEvent := createEvent("default", "test_tick", "tick", testReference)
-	_, err = core.CreateEvent(context.Background(), tickEvent)
+	tickEvent := createEvent("default", "test_tick", "tick", *testReference)
+	_, err = core.Events("default").Create(tickEvent)
 	require.Nil(suite.T(), err)
 
 	// Test we get the new tick event
@@ -122,19 +118,19 @@ func (suite *testSuite) TestKubeEvents() {
 	require.Nil(suite.T(), err)
 	assert.Len(suite.T(), added, 1)
 	assert.Len(suite.T(), modified, 0)
-	assert.Equal(suite.T(), "tick", *added[0].Reason)
+	assert.Equal(suite.T(), "tick", added[0].Reason)
 
 	// Update tick event
 	pointer2 := int32(2)
 	tickEvent2 := added[0]
-	tickEvent2.Count = &pointer2
-	tickEvent3, err := core.UpdateEvent(context.Background(), tickEvent2)
+	tickEvent2.Count = pointer2
+	tickEvent3, err := core.Events("default").Update(tickEvent2)
 	require.Nil(suite.T(), err)
 
 	// Update tick event a second time
 	pointer3 := int32(3)
-	tickEvent3.Count = &pointer3
-	_, err = core.UpdateEvent(context.Background(), tickEvent3)
+	tickEvent3.Count = pointer3
+	_, err = core.Events("default").Update(tickEvent3)
 	require.Nil(suite.T(), err)
 
 	// Test we get the two modified test events
@@ -142,11 +138,11 @@ func (suite *testSuite) TestKubeEvents() {
 	require.Nil(suite.T(), err)
 	assert.Len(suite.T(), added, 0)
 	assert.Len(suite.T(), modified, 2)
-	assert.Equal(suite.T(), "tick", *modified[0].Reason)
-	assert.EqualValues(suite.T(), 2, *modified[0].Count)
-	assert.Equal(suite.T(), "tick", *modified[1].Reason)
-	assert.EqualValues(suite.T(), 3, *modified[1].Count)
-	assert.EqualValues(suite.T(), *modified[0].Metadata.Uid, *modified[1].Metadata.Uid)
+	assert.Equal(suite.T(), "tick", modified[0].Reason)
+	assert.EqualValues(suite.T(), 2, modified[0].Count)
+	assert.Equal(suite.T(), "tick", modified[1].Reason)
+	assert.EqualValues(suite.T(), 3, modified[1].Count)
+	assert.EqualValues(suite.T(), modified[0].InvolvedObject.UID, modified[1].InvolvedObject.UID)
 
 	// We should get nothing new now
 	added, modified, resversion, err = suite.apiClient.LatestEvents(resversion)
